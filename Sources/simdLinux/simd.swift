2abbbc82863942be1f1dc9b223213560bcecb8c5
Jochen Kiemes <jochen@kiemes.de>
2021-10-31 11:59:55 +0100

    add simd_dot for simd_double3

--- Sources/simdLinux/simd.swift
+++ Sources/simdLinux/simd.swift
@@ -7,3 +7,6 @@ public struct simd_double3: Decodable {
     public var y: Double
     public var z: Double
 }
+public func simd_dot(_ x: simd_double3, _ y: simd_double3) -> Double {
+    return x.x*y.x + x.y*y.y + x.z*y.z
+}
