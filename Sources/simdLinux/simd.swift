3f7586ec3fce618c2aec3e4dbc749cdf42f28b20
Jochen Kiemes <jochen@kiemes.de>
2021-10-31 11:58:00 +0100

    define simd_double2 and simd_double3 as Decodable

--- Sources/simdLinux/simd.swift
+++ Sources/simdLinux/simd.swift
@@ -1,8 +1,8 @@
-public struct simd_double2 {
+public struct simd_double2: Decodable {
     public var x: Double
     public var y: Double
 }
-public struct simd_double3 {
+public struct simd_double3: Decodable {
     public var x: Double
     public var y: Double
     public var z: Double
