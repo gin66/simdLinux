116d09a2dce245d2c58d5fbff0b2fb32cdac3d63
Jochen Kiemes <jochen@kiemes.de>
2021-10-31 12:57:33 +0100

    simd_double3 as hashable

--- Sources/simdLinux/simd.swift
+++ Sources/simdLinux/simd.swift
@@ -24,7 +24,7 @@ public extension simd_double2 {
     }
 }
 
-public struct simd_double3: Codable {
+public struct simd_double3: Codable, Hashable {
     public var x: Double
     public var y: Double
     public var z: Double
