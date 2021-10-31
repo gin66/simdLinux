bf86f140e0d8e6ac8f03530917b4012f6612c593
Jochen Kiemes <jochen@kiemes.de>
2021-10-31 11:54:16 +0100

    define simd_double2 and simd_double3

--- Sources/simdLinux/simd.swift
+++ Sources/simdLinux/simd.swift
@@ -1,3 +1,9 @@
-struct simd {
-    var text = "Hello, World!"
+public struct simd_double2 {
+    public var x: Double
+    public var y: Double
+}
+public struct simd_double3 {
+    public var x: Double
+    public var y: Double
+    public var z: Double
 }
