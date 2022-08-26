30c54c41802691c81374d4e8b286dd8603f4162b
Jochen Kiemes <jochen@kiemes.de>
2022-08-26 18:52:56 +0200

    generalized functions not limited to double

--- Sources/simdLinux/SpecializedFunctions.swift
+++ Sources/simdLinux/SpecializedFunctions.swift
@@ -5,38 +5,30 @@ public func simd_cross<T: FloatingPoint>(_ left: SIMD3<T>, _ right: SIMD3<T>) ->
     let y = left.z*right.x - left.x*right.z
     let z = left.x*right.y - left.y*right.x
     let res = SIMD3<T>(x, y, z)
-//    if recording.enable {
-//        let entry = SimdRecordingCross(p1: left, p2: right, res: res)
-//        recording.append(cross: entry)
-//    }
     return res
 }
 
-public func simd_act(_ quat: simd_quatd, _ vector: simd_double3) -> simd_double3 {
+public func simd_act<T>(_ quat: QUAT4<T>, _ vector: SIMD3<T>) -> SIMD3<T> {
     // https://en..m.wijipedia.org/wiki/Euler-Rodrigues_formula
     // alternative: https://gamedev.stackexchange.com/questions/28395/rotating-vector3-by-a-quaternion
-    let w = simd_double3(quat.ix, quat.iy, quat.iz)
+    let w = SIMD3<T>(quat.ix, quat.iy, quat.iz)
     let w_x = simd_cross(w, vector)
     let w_w_x = simd_cross(w, w_x)
     let res = vector + 2*quat.r*w_x + 2*w_w_x
-//    if recording.enable {
-//        let entry = SimdRecordingAct(p1: quat, p2: vector, res: res)
-//        recording.append(act: entry)
-//    }
     return res
 }
 
-public func simd_quaternion(_ ang: Double, _ vector: simd_double3) -> simd_quatd {
+public func simd_quaternion<T: Numeric>(_ ang: Double, _ vector: SIMD3<T>) -> QUAT4<T> {
     let n = simd_normalize(vector)
-    let c = cos(ang/2)
-    let s = sin(ang/2)
+    let c = cos(ang/2) as! T
+    let s = sin(ang/2) as! T
     let ix = n.x*s
     let iy = n.y*s
     let iz = n.z*s
-    return simd_quatd(ix, iy, iz, c)
+    return QUAT4(ix, iy, iz, c)
 }
 
-public func simd_mul(_ left: simd_quatd, _ right: simd_quatd) -> simd_quatd {
+public func simd_mul<T>(_ left: QUAT4<T>, _ right: QUAT4<T>) -> QUAT4<T> {
     // Hamilton product https://en.wikipedia.org/wiki/Quaternion
     let a1 = left.r
     let a2 = right.r
@@ -50,10 +42,6 @@ public func simd_mul(_ left: simd_quatd, _ right: simd_quatd) -> simd_quatd {
     let ix = a1*b2 + b1*a2 + c1*d2 - d1*c2
     let iy = a1*c2 - b1*d2 + c1*a2 + d1*b2
     let iz = a1*d2 + b1*c2 - c1*b2 + d1*a2
-    let res = simd_quatd(ix, iy, iz, r)
-//    if recording.enable {
-//        let entry = SimdRecordingMul(p1: left, p2: right, res: res)
-//        recording.append(mul: entry)
-//    }
+    let res = QUAT4(ix, iy, iz, r)
     return res
 }
